1409b5e62cce01aca4fb6c8bbb0539e96eb0461c
Ondra Janoška <o.janoska98@gmail.com>
2023-03-30 14:54:50 +0200

    try at ray cassting

--- Character.gd
+++ Character.gd
@@ -1,21 +1,50 @@
 extends CharacterBody2D
 
+@onready var collisionMask = self.collision_mask
 var mousePosition = Vector2.ZERO
 var toMouse = Vector2.ZERO
 const MAXSPEED = 300
 const CLOSEST_APPROACH_SQUARED = 10
+const MAX_CHECK_ANGLE = PI/4
+const SOD = preload("res://SecondOrderDynamics.gd")
+var DynamicsHandler : SOD
+@export_range(-1,2) var f:float = 2
+@export_range(-1,2) var z:float = 2
+@export_range(-1,2) var r:float = 0
+
+@onready var hipNode = get_node("HipJoint")
+@onready var footNode = get_node("UpperLeg/LowerLeg/Foot")
+@onready var legVector = hipNode.global_position - footNode.global_position
+@onready var LEG_LENGTH = legVector.length()
 
 
-func _physics_process(_delta):
-	
-	
-	# move to mouse and if close enough dont move at all
+func _ready():
+	DynamicsHandler = SOD.new(f, z, r, global_position)
+
+func _physics_process(delta):
 	mousePosition = get_global_mouse_position()
-	toMouse = mousePosition-global_position
-	if toMouse.length_squared()<CLOSEST_APPROACH_SQUARED:
-		toMouse = Vector2.ZERO
-	if toMouse.length_squared()>1:
-		toMouse = toMouse.normalized()
-	velocity = toMouse*MAXSPEED
+	global_position = DynamicsHandler.UpdateViaSOD(delta, mousePosition)
 	
-	move_and_slide()
+	var space_state = get_world_2d().direct_space_state
+	
+	var checkResult = null
+	legVector = hipNode.global_position - footNode.global_position
+	var legAngle = legVector.angle()+0.5*PI
+	if legAngle > MAX_CHECK_ANGLE:
+		checkResult = checkForGround(space_state, -MAX_CHECK_ANGLE)
+	elif legAngle < -MAX_CHECK_ANGLE:
+		checkResult = checkForGround(space_state, MAX_CHECK_ANGLE)
+	if checkResult != null && not checkResult.is_empty():
+		print(checkResult)
+
+
+func checkForGround(space_state, check_angle):
+	var hipPosition = hipNode.global_position
+	var checkDirection = Vector2.DOWN.rotated(-check_angle)*LEG_LENGTH
+	var query = PhysicsRayQueryParameters2D.create(hipPosition, hipPosition+checkDirection, collisionMask)
+	var result = space_state.intersect_ray(query)
+	
+	return result
+
+func step():
+	pass
--- UpperLeg.gd
+++ UpperLeg.gd
@@ -1,19 +1,9 @@
 extends CharacterBody2D
 
-const SOD = preload("res://SecondOrderDynamics.gd")
-var DynamicsHandler : SOD
-@export_range(-1,1) var f:float = 0
-@export_range(-1,1) var z:float = 0
-@export_range(-1,1) var r:float = 0 
-@onready var parentNode = get_parent()
 
-func _ready():
-	DynamicsHandler = SOD.new(f, z, r, global_position)
+const ROTATION_SPEED = 2.2
 
 func _process(delta):
-	var parentPosition = parentNode.global_position
-	var parentVelocity = parentNode.velocity
-	global_position = DynamicsHandler.UpdateViaSOD(delta, parentPosition, parentVelocity)
-	
-	
-
+	var leftPressed = int(Input.is_action_pressed("ui_left"))
+	var rightPressed = int(Input.is_action_pressed("ui_right"))
+	rotate(ROTATION_SPEED*delta*(leftPressed-rightPressed))
